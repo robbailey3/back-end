f33c1d10530e4fbc7a19c33efdb163812be768e8
Rob Bailey <rob.bailey3@gmail.com>
2019-03-19 21:14:51 +0000

    Initial Build: Adding in services and some new components

--- src/app/global/global.module.ts
+++ src/app/global/global.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavigationComponent, FooterComponent],
   imports: [
     CommonModule
   ]
