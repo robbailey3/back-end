8cab5072f047e7c9313880470c68a1c0f4b2cde6
Rob Bailey <rob.bailey3@gmail.com>
2019-03-28 21:25:07 +0000

    Adding in button styling

--- src/app/dashboard/todo-list/todo-list.component.ts
+++ src/app/dashboard/todo-list/todo-list.component.ts
@@ -1,13 +1,12 @@
 import { APIResponse } from './../../shared/interfaces/api-response';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from './todo.service';
 import { Todo } from './todo';
 
 @Component({
   selector: 'rb-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
   todos: Todo[];
